f804703be8762215f9a7f2f25fa18b31d30a8872
philippe v <philippe_vanko@hotmail.fr>
2022-04-11 14:40:44 +0200

    maj webpack
    maj fichier jsonet css

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,7 +6,7 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import '../styles/app.css';
+import '../styles/app.scss';
 
-// start the Stimulus application
-import '../bootstrap';
+// Chargement de la partie JS de bootstrap
+require('bootstrap');
