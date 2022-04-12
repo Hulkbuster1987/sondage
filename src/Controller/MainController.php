b02e2f44f2d4b8ec779d5aace05dfe2f79d3b6e8
philippe v <philippe_vanko@hotmail.fr>
2022-04-12 10:25:05 +0200

    form sureys
    ajout formulaire questionnaire

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -5,13 +5,16 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Form\SurveysType;
 
 class MainController extends AbstractController
 {
     #[Route('/main', name: 'app_main')]
-    public function index(): Response
+    public function createSurveys(): Response
     {
-        return $this->render('main/index.html.twig', [
+        $newSurveys = new Surveys();
+        $form = $this->createForm(CreateSurveysFormType::class);
+        return $this->render('templates/surveys/index.html.twig', [
             'controller_name' => 'MainController',
         ]);
     }
