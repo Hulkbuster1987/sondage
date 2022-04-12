4f41014ef4a51b6356410c356c48cbcf07031187
philippe v <philippe_vanko@hotmail.fr>
2022-04-12 10:42:49 +0200

    update main controller
    update

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -5,16 +5,13 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Form\SurveysType;
 
 class MainController extends AbstractController
 {
     #[Route('/main', name: 'app_main')]
-    public function createSurveys(): Response
+    public function index(): Response
     {
-        $newSurveys = new Surveys();
-        $form = $this->createForm(CreateSurveysFormType::class);
-        return $this->render('templates/surveys/index.html.twig', [
+        return $this->render('main/index.html.twig', [
             'controller_name' => 'MainController',
         ]);
     }
