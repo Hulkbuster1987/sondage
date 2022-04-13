114e0e38fa849a1cb74ba90511e21542ac9388cd
jeanmarcandre <93780722+jeanmarcandre@users.noreply.github.com>
2022-04-13 14:49:14 +0200

    'toto'

---
+++ src/Entity/UserType.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserTypeRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: UserTypeRepository::class)]
+class UserType
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
